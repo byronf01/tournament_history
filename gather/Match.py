1faa32b8db93317ada83c33a150ba2108aaf8d88
Byron Fong <byron.fong@gmail.com>
2023-04-08 13:18:00 -0700

    base for Match.py

--- gather/Match.py
+++ gather/Match.py
@@ -8,7 +8,7 @@ CLIENT_SECRET = os.getenv('client-secret')
 
 class Match:
 
-    def __init__(self, id: int):
+    def __init__(self, id: int, teamName: str, multipliers: dict):
         
         # API data
         self.__apiData = {
@@ -21,30 +21,46 @@ class Match:
         # Match ID
         self.__id = id
 
+        # the user's team name
+        self.__teamName = teamName
+
         # Name of the match
         self.__name = "" 
+
+        # Stores collection of match multipliers, should be supplied by tournament obj
+        # Ex: {"EZ": 1.80, "HD": 0.96}
+        self.__multipliers = multipliers
+
+        # outcome of the match, blue - red
+        self.__result = (0, 0)
+
+        # team type of the match
+        self.__teamType = ""
 
         # Stores a list of events that happened during the match
         self.__events = {}
 
-        # outcome of the match
-        self.__result = (0, 0)
+        # Stores players on each team. If teamType is head-to-head, default {team_blue: your_user, team_red: opponent}
+        self.__teams = {}
 
         # stores collection of match costs per player
         self.__matchcosts = {} 
-
-
-        self.__events = self.__getEvents()
-
         
+        a = input("Please review mp manually and enter # warmups, # extraneous maps at end, map # of aborts separated by commas")
+        b = input()
+        c = input()
+        self.__warmups = 2 if a == "" else int(a)
+        self.__end = 0 if b == "" else int(b)
+        self.__aborts = [] if c == "" else [int(foo) for foo in c.split(",")]
+        
+        self.__events = self.__process()
 
-    def __getEvents(self):
+    def __process(self):
         """
-        Returns a dictionary of maps that were played in the match and details of each map. Also updates the name 
-        of the Match object without making another API call.
+        Main function that proccesses the mp. Returns a dictionary of maps that were played in the match and details of each map. 
         """
         try:
-            response = requests.post('https://osu.ppy.sh/oauth/token', data=data)
+            response = requests.post('https://osu.ppy.sh/oauth/token', data=self.__apiData)
             token = response.json().get('access_token')
 
             headers = {
@@ -58,12 +74,59 @@ class Match:
         except:
             print("API failure")
             raise ValueError()
+        
+        # print(info)
 
-        self.__name = info['match']['name'] 
+        self.__name = info['match']['name'] # finds and enters match name 
 
+        # keeps track of which matches to ignore with ignore, and the team type of the match
+        team_type_counts = {"head-to-head": 0, "team-vs": 0}
+        total = 0
         for event in info['events']:
             if event['detail']['type'] == 'other':
+                total += 1
+                team_type_counts[ event['game']['team_type'] ] += 1
+        ignore = []
+        for i in range(1, self.__warmups + 1):
+            ignore.append(i)
+        ignore.extend(self.__aborts)
+        for i in range(total, total - self.__end, -1):
+            ignore.extend(i)
+        self.__teamType = 'team-vs' if team_type_counts['team-vs'] >= team_type_counts['head-to-head'] else 'head-to-head'
+        
+        maps_played = 0
+        index = 0
+        team_check = {} # form {player: {blue: x, red: y}}
+        for event in info['events']:
+            if event['detail']['type'] == 'other':
+                maps_played += 1
+                if maps_played in ignore:
+                    continue
+                index += 1
 
+                map_link = "https://osu.ppy.sh/b/" + str(event['game']['beatmap']['id'])
+                map_background = event['game']['beatmap']['beatmapset']['covers']['cover']
+                map_title = event['game']['beatmap']['']
+                scores = event['game']['scores']
+                
+                # Process list of scores
+                if self.__teamType == 'team-vs':
 
+                    blue_score = 0
+                    red_score = 0
+                    blue_team = set()
+                    red_team = set()
+                    for s in scores:
+
+                        
+
+                elif self.__teamType == 'head-to-head':
+
+            
+                
+                    
     def getMatch():
         return None # what should be the key for match? match title?
+
+if __name__ == "__main__":
+    Match(107542811, "Kusudama")
