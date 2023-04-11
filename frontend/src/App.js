1919359dbe26ab767d18dd4c727a9d419716c13d
Byron Fong <byron.fong@gmail.com>
2023-04-11 11:45:56 -0700

    start home page of app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
+import tourn from './trophy.png';
 import './App.css';
 
+function Tag() {
+  return (
+    <div style={{
+      margin: '0 auto',
+      width: '60%',
+      height: '18em',
+      border: 'hidden',
+    }}>
+      <img src='https://a.ppy.sh/16626263?1677187336.png' class="responsive"></img>
+      <p style={{
+        color: 'white',
+        fontFamily: 'trebuchet ms',
+        fontSize: '4em',
+      }}>hiyah's Tournament History</p>
+    </div>
+  )
+}
+
+function HomeLine() {
+  return (
+    <hr style={{
+      backgroundColor: '#dae4f0',
+      width: '70%',
+      height: '1em',
+    }}/>
+  )
+}
+
+function TournamentsButton() {
+  return (
+    <button class="b1">
+      Tournaments<br></br>
+      <img src={tourn} alt="trophy" class="responsive"></img>
+    </button>
+  )
+}
+
 function App() {
+  document.body.style = 'background: #617285;';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{height: '100px'}}></div>
+      <Tag />
+      <div style={{height: '120px'}}></div>
+      <HomeLine />
+      <TournamentsButton />
     </div>
+    
   );
 }
 
