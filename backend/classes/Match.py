1ee20c883e1d2dea5236a6c0e435cd92f0eb4bd0
Byron Fong <byron.fong@gmail.com>
2023-04-13 13:27:07 -0700

    idk

--- backend/classes/Match.py
+++ backend/classes/Match.py
@@ -1,10 +1,12 @@
 import os, requests, json, re, time
 from dotenv import load_dotenv
+from Score import Score
 
 load_dotenv()
 
 API_BASE_URL = "https://osu.ppy.sh/api/v2/"
 CLIENT_SECRET = os.getenv('client-secret')
+DEFAULT_USER = 16626263
 
 class Match:
 
@@ -31,14 +33,14 @@ class Match:
         # Ex: {"EZ": 1.80, "HD": 0.96}
         self.__multipliers = multipliers
 
-        # outcome of the match, blue - red
+        # outcome of the match, blue - red. If head-to-head, your user is always first.
         self.__result = [0, 0]
 
         # team type of the match
         self.__teamType = ""
 
         # Stores a list of events that happened during the match
-        self.__events = {}
+        self.__events = []
 
         # Stores players on each team. If teamType is head-to-head, default {team_blue: your_user, team_red: opponent}
         self.__teams = {}
@@ -54,7 +56,7 @@ class Match:
         self.__warmups = 2 if a == "" else int(a)
         self.__end = 0 if b == "" else int(b)
         
-        self.__events = self.__process()
+        self.__process()
 
     def __apiCall(self):
         """
@@ -89,6 +91,10 @@ class Match:
         prev_id = 0
         for event in info['events']:
             if event['detail']['type'] == 'other':
+                # If scores is empty, map was aborted early and does not show up in mp. 
+                if event['game']['scores'] == []:
+                    continue 
+
                 event_ct += 1
                 team_type_counts[ event['game']['team_type'] ] += 1
 
@@ -107,20 +113,32 @@ class Match:
         self.__teamType = 'team-vs' if team_type_counts['team-vs'] >= team_type_counts['head-to-head'] else 'head-to-head'
         return ignore
     
+    def __calculateTeams(self, team_check: dict):
+        """
+        Figures out who is on which team from the team_check dict generated in __process. If the match type is 
+        head-to-head, always defaults your user to blue team. 
+        """
+        self.__teams['team_blue'] = []
+        self.__teams['team_red'] = []
+        for user, teams in team_check.items():
+            if teams['red'] >= teams['blue']:
+                self.__teams['team_red'].append(user)
+            elif teams['blue'] > teams['red']:
+                self.__teams['team_blue'].append(user)
+
+    
     def __process(self):
         """
-        Main function that proccesses the mp. Returns a dictionary of maps that were played in the match and details of each map. 
+        Main function that proccesses the mp. Parses maps that were played in the match and details of each map. 
+        Adds to self.__events in the order that the maps were played.
         """
         info = self.__apiCall()
-        
-        print(info)
 
         # finds and enters match name 
         self.__name = info['match']['name'] 
 
         # keeps track of which matches to ignore with ignore, and the team type of the match
         ignore = self.__preprocess(info)
-        print(ignore)
         
         maps_played = 0
         index = 0
@@ -146,31 +164,105 @@ class Match:
                     # Filter out events that are not team-vs
                     if event['game']['team_type'] != 'team-vs': continue
 
-                    blue_score = 0
-                    red_score = 0
-                    blue_team = set()
-                    red_team = set()
+                    blue_total = 0
+                    red_total = 0
+                    blue_scores = {}
+                    red_scores = {}
+                    
                     for s in scores:
                         
+                        new_score = Score(s)
+
                         # Process score with multipliers, use default multipliers if none specified
+                        for mod in new_score.mods:
+                            if mod in multipliers:
+                                new_score.value *= multipliers[mod]
+
+                        # Add player to team_check dict if this is their first map
+                        if new_score.player not in team_check:
+                            team_check[new_score.player] = {'blue': 0, 'red': 0}
 
                         # Determine team that the score belongs to
-                        pass
+                        team = new_score.team
+                        if team == 'red':
+                            red_total += new_score.value
+                            formatted_score = new_score.getScore()
+                            red_scores[formatted_score[0]] = formatted_score[1]
+                        elif team == 'blue':
+                            blue_total += new_score.value
+                            formatted_score = new_score.getScore()
+                            blue_scores[formatted_score[0]] = formatted_score[1]
                         
+                        # determine team of the player and add to team_check
+                        team_check[new_score.player][team] += 1
                         
-                        # Determine team of the player and add to set
-
-                    
                     # Compare red - blue scores, add 1 to match result for winning team
+                    if blue_total > red_total:
+                        self.__result[0] += 1
+                    elif red_total > blue_total:
+                        self.__result[1] += 1
 
-                    # add players to team check
-                        
+                # Add entire event to self.events
+                new_event = {
+                    "map-background": map_background,
+                    "map-title": map_title,
+                    "map-link": map_link,
+                    "red_scores": red_scores,
+                    "blue_scores": blue_scores,
+                    "red_total": red_total,
+                    "blue_total": blue_total
+                }
 
-                elif self.__teamType == 'head-to-head':
+                self.__events.append(new_event)
 
-                    # Filter out events that are not head-to-head
-                    if event['game']['team_type'] != 'team-vs': continue
-                    pass
+            elif self.__teamType == 'head-to-head':
+
+                # Filter out events that are not head-to-head
+                if event['game']['team_type'] != 'team-vs': continue
+                
+                user_score = 0
+                opponent_score = 0
+                blue_scores = {}
+                red_scores = {}
+                for s in scores:
+
+                    new_score = Score(s)
+
+                    # Process score with multipliers, use default multipliers if none specified
+                    for mod in new_score.mods:
+                        if mod in multipliers:
+                            new_score.value *= multipliers[mod]
+                    
+                    if new_score.player == DEFAULT_USER:
+                        user_score += new_score.value
+                        formatted_score = new_score.getScore()
+                        blue_scores[formatted_score[0]] = formatted_score[1]
+                        team_check[new_score.player] = {'blue': 1, 'red': 0}
+                    else:
+                        opponent_score += new_score.value
+                        formatted_score = new_score.getScore()
+                        red_scores[formatted_score[0]] = formatted_score[1]
+                        team_check[new_score.player] = {'blue': 0, 'red': 1}
+
+                if user_score > opponent_score:
+                    self.__result[0] += 1
+                else:
+                    self.__result[1] += 1
+
+                new_event = {
+                    "map-background": map_background,
+                    "map-title": map_title,
+                    "map-link": map_link,
+                    "red_scores": red_scores,
+                    "blue_scores": user_score,
+                    "red_total": opponent_score,
+                    "blue_total": user_score
+                }
+
+                self.__events.append(new_event)
+
+        self.__calculateTeams()
+
                       
     def getMatch(self):
         return {self.__id: {
@@ -179,11 +271,11 @@ class Match:
                 "multipliers": self.__multipliers,
                 "result": self.__result,
                 "matchcosts": [{}, {}], # TO-DO
-                "events": {}
+                "events": self.__events
             }
         } # what should be the key for match? match title? match id? should there even be a key?
 
 if __name__ == "__main__":
     multipliers = {"EZ": 1.8}
-    m = Match('107542811', "hiyah", multipliers)
+    m = Match('103526237', "hiyah", multipliers)
     print(m.getMatch())
--- backend/classes/Score.py
+++ backend/classes/Score.py
@@ -4,11 +4,27 @@ class Score:
 
     def __init__(self, score_info: dict):
 
-        self.__acc = score_info['accuracy']
-        self.__combo = score_info['combo']
-        self.__mods = score_info['mods']
-        self.__value = score_info['score']
+        self.acc = score_info['accuracy']
+        self.combo = score_info['max_combo']
+        self.mods = score_info['mods']
+        self.value = score_info['score']
         a = score_info['statistics']
-        self.__stats = {'count_100': a['count_100'], 'count_300': a['count_300'], 
+        self.stats = {'count_100': a['count_100'], 'count_300': a['count_300'], 
                         'count_50': a['count_50'], 'count_miss': a['count_miss']}
-        self.__team = score_info['match']['team']
+        self.player = score_info['user_id']
+        self.team = score_info['match']['team']
+
+    def getScore(self):
+        """
+        Returns 2-tuple of the player who set the score and the score details
+        """
+        return (self.player, {
+                "acc": self.acc,
+                "combo": self.combo,
+                "mods": self.mods,
+                "value": self.value,
+                "stats": self.stats, 
+                "team": self.team
+            } )
+                
+        
