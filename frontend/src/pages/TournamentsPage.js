a1daf26ec4f3a98748dfdcce06cbb2a1129e1798
Byron Fong <byron.fong@gmail.com>
2023-04-22 17:04:10 -0700

    trying to figure out DOTS

--- frontend/src/pages/TournamentsPage.js
+++ frontend/src/pages/TournamentsPage.js
@@ -4,13 +4,25 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 import React from 'react';
 import { Component } from 'react';
 import Navbar from '../components/Navbar'
+import Pagination from '../components/Pagination'
 
+let PageSize = 10;
 
-
+/*
 class TournamentsPage extends Component {
-    
+
+
+
 render() {
+
+    const [currentPage, setCurrentPage] = useState(1);
     
+    const currentTableData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return data.slice(firstPageIndex, lastPageIndex);
+      }, [currentPage]);
+
     return (
     <div>
         <Navbar />
@@ -21,6 +33,13 @@ render() {
             fontSize: "50px",
             color: "rgb(255,255,255)"}}>🏆 Tournaments 🏆</h1>
             </div>
+            <Pagination 
+                className="pagination-bar"
+                currentPage={currentPage}
+                totalCount={data.length}
+                pageSize={PageSize}
+                onPageChange={page => setCurrentPage(page)}/>
+
         </div>
         
         
@@ -29,5 +48,40 @@ render() {
     )
 }
 }
-  
+*/
+function TournamentsPage() {
+    const [currentPage, setCurrentPage] = useState(1);
+        
+    const currentTableData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return data.slice(firstPageIndex, lastPageIndex);
+        }, [currentPage]);
+    
+    return (
+        <div>
+            <Navbar />
+            <div style={{paddingLeft: "10%", 
+                paddingRight: "10%"}}>
+                <div>
+                <h1 style={{textAlign: "center", 
+                fontSize: "50px",
+                color: "rgb(255,255,255)"}}>🏆 Tournaments 🏆</h1>
+                </div>
+                <Pagination 
+                    className="pagination-bar"
+                    currentPage={currentPage}
+                    totalCount={data.length}
+                    pageSize={PageSize}
+                    onPageChange={page => setCurrentPage(page)}/>
+    
+            </div>
+            
+            
+        </div>
+
+        
+    )
+}
+      
 export { TournamentsPage };
