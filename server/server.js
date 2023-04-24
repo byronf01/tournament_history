143aca391d2ca0751f91764350b2d352c0c6cda7
Byron Fong <byron.fong@gmail.com>
2023-04-23 22:28:54 -0700

    server

--- server/server.js
+++ server/server.js
@@ -1,36 +1,34 @@
 const mongoose = require("mongoose");
-const express = require('express'); // do we need these last two
+const express = require('express');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const PASSWORD = process.env.PASSWORD
-const URI = "mongodb+srv://byronfong:" + PASSWORD + "@tournament-history.qp41sza.mongodb.net/?retryWrites=true&w=majority"
-
-connect(URI).catch(err => console.log(err));
 const tournSchema = new mongoose.Schema({ });
 const Tourn = mongoose.model('tournament_history', tournSchema);
 
-app.get('/api/data', async (req, res) => {
-    const data = await Tourn.find((err, tourns) => {
-        if (err) {
-            console.error(err);
-        } else {
-            data = tourns
-        }
-        });
-    res.json(data)
+app.get("/api/data", (req, res) => {
+
+    const PASSWORD = process.env.mongo-password
+    console.log(PASSWORD)
+    const URI = "mongodb+srv://byronfong:" + PASSWORD + "@tournament-history.qp41sza.mongodb.net/?retryWrites=true&w=majority"
+    connect(URI).catch(err => console.log(err));
+
+    getItems().then( (foundItems) => {
+        res.json(foundItems);
+    });
+
+    
 })
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000')
-})
+async function getItems() {
+    const docs = await Tourn.find({});
+    return docs;
+}
 
 async function connect(URI) {
     const connection = await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log(`MongoDB Connected: ${connection.connection.host}`)
 }
 
-
-
-export default connect;
+app.listen(5000, () => {console.log("Server started on port 5000")})
