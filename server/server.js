8a57f7a7e4367c09843b61ed1a755125c19161c8
Byron Fong <byron.fong@gmail.com>
2023-04-25 17:16:39 -0700

    the backend works now

--- server/server.js
+++ server/server.js
@@ -4,30 +4,30 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const tournSchema = new mongoose.Schema({ });
-const Tourn = mongoose.model('tournament_history', tournSchema, 'tournament_history');
+const TournSchema = mongoose.Schema({ any: {} }, { collection: "tournament_history" });
+const tournament_history = mongoose.model("tournament_history", TournSchema, "tournament_history");
 require('dotenv').config({path:__dirname+'../../.env'});
 const PASSWORD = process.env.mongo_password
+console.log(tournament_history)
 
-app.get("/api/data", (req, res) => {
+app.get("/api/data", async (req, res) => {
 
-    const URI = "mongodb+srv://byronfong:" + PASSWORD + "@tournament-history.qp41sza.mongodb.net/?retryWrites=true&w=majority"
-    connect(URI).catch(err => console.log(err));
+    const URI = `mongodb+srv://byronfong:${PASSWORD}@tournament-history.qp41sza.mongodb.net/tournament_history`
+    await connect(URI).catch(err => console.log(err));
 
     getItems().then( (foundItems) => {
         res.json(foundItems);
     });
 
     
+    
 })
 
 async function getItems() {
-    const res = await Tourn.find({}).
-        then(tourn => {
-            console.log(tourn);
-            return tourn;
-        })
-    return res;
+    const query = await tournament_history.find()
+    query instanceof mongoose.Query;
+    const docs = await query
+    return docs    
 }
 
 async function connect(URI) {
