421e01aa1e6ccfc950faa51fba06af867bf457d8
Byron Fong <byron.fong@gmail.com>
2023-04-27 18:30:42 -0700

    figure out img fitting to div

--- client/src/components/TournamentPreview/TournamentPreview.js
+++ client/src/components/TournamentPreview/TournamentPreview.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './index.css';
 
 function TournamentPreview( {new_key, new_data} ) {
     // Data should be an Obj
@@ -21,6 +22,7 @@ function TournamentPreview( {new_key, new_data} ) {
     // Forum post
     // console.log(data)
     let start = data[name]['date_f'].split("T")[0]
+    let img = data[name]['banner']
     let info = ""
     
     if (data[name]['forum'] != "") {
@@ -31,14 +33,29 @@ function TournamentPreview( {new_key, new_data} ) {
         info = "No Preview";
     }
     
-
     return (
         <a href={data[name]['forum']} style={{textDecoration: "none"}}>
-          <div style={{border: "1px solid black", borderStyle: "solid", margin: "10px 0"}}>
-            <div style={{padding: "3%"}}>
-              <h1 style={{userSelect: "none", color: "black"}}>{name}</h1>
-              <p style={{userSelect: "none", color: "black"}}>{start}</p>
-              <p style={{userSelect: "none", color: "black"}}>{info}</p>
+          <div class="background" style={{background: {img}}}>
+            <div class="inner" style={{border: "1px solid black", borderStyle: "solid", 
+                        margin: "30px 0", borderRadius: "30px",
+                        width: "97%", backgroundColor: "#BEC5AD",
+                        display: "flex", maxHeight: "200px",
+                        boxShadow: "5px 10px #1a1d21"}}>
+              <div style={{width: "40%", height: "100%", display: "block", 
+                          overflow: "auto", objectFit: "cover"}}>
+                
+                <img src={img} style={{maxWidth: "100%", 
+                                      maxHeight: "100%",
+                                      objectFit: "cover"
+                                      }}/>
+              </div>
+              <div style={{}}>
+                <div style={{}}>
+                  <h1 style={{userSelect: "none", color: "black"}}>{name}</h1>
+                  <p style={{userSelect: "none", color: "black"}}>{start}</p>
+                  <p style={{userSelect: "none", color: "black"}}>{info}</p>
+                </div>
+              </div>
             </div>
           </div>
         </a>
