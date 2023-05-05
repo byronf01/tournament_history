59cbd12f857bb84600fae486b0efd6dd3318e77d
Byron Fong <byron.fong@gmail.com>
2023-05-04 22:47:36 -0700

    matches page finisihed

--- client/src/components/MatchPreview/MatchPreview.js
+++ client/src/components/MatchPreview/MatchPreview.js
@@ -8,12 +8,13 @@ function MatchPreview( {acronym, mp, stage, match_name} ) {
             <a href={`/matches/${acronym}/${mp}`} style={{textDecoration: "none"}}>
             <div style={{display: "flex", border: "1px solid black", width: "90%", height: "100px",
                         justifyContent: "space-between", alignItems: "center"}}>
-                <div style={{display: "flex", marginLeft: "10%", paddingRight: "10%", borderRight: "1px dotted black", width: "20%", height: "100%"}}>
-                    <div style={{display: "flex", alignItems: "center", justifyContent: "center", color: "black"}}>{stage}</div>
-                </div>
                 <div style={{display: "flex", marginRight: "10%", height: "100%", width: "50%"}}>
                     <div style={{display: "flex", alignItems: "center", justifyContent: "center", color: "black"}}>{match_name}</div>
                 </div>
+                <div style={{display: "flex", marginLeft: "10%", paddingRight: "10%", borderLeft: "1px dotted black", width: "20%", height: "100%"}}>
+                    <div style={{display: "flex", alignItems: "center", justifyContent: "center", color: "black"}}>{stage}</div>
+                </div>
+                
             </div>
             </a>
         </div>
--- client/src/components/MatchesBlock/MatchesBlock.js
+++ client/src/components/MatchesBlock/MatchesBlock.js
@@ -1,25 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import MatchPreview from '../TournamentPreview'
+import MatchPreview from '../MatchPreview'
 
 function MatchesBlock( {matches} ) {
     const [currentData, setcurrentData] = useState("");
     const [empty, setEmpty] = useState(true);
     
     let block = [];
-    /*
     useEffect ( () => {
         if (matches.includes()) {
             setEmpty(true)
         } else {
             block.length = 0
-            for (let i = 0; i < tourns.length; i++) {
-                block.push(<MatchPreview new_key={Object.keys(tourns[i])[0]} new_data={tourns[i]} />)
+            for (let i = 0; i < matches.length; i++) {
+                block.push(<MatchPreview acronym={matches[i]['acronym']} mp={matches[i]['mp']} stage={matches[i]['stage']} match_name={matches[i]['match_name']}/>)
             }
             setcurrentData(block)
             setEmpty(false)
         }
-    }, [tourns])
-    */
+    }, [matches])
+    
 
     return (
         <div>
