ae21c65325f8beb9b3083f926187e2a8020899db
Byron Fong <byron.fong@gmail.com>
2023-06-09 00:38:54 -0700

    minor fix

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -18,7 +18,7 @@ import Tag from '../components/Tag'
             
           </div>
           <div style={{position: 'absolute', bottom: 0, right: 0}}>
-            <a href="https://info.flagcounter.com/xOdr">
+            <a href="https://info.flagcounter.com/xOdr" target="_blank" rel="noreferrer">
               <img src="https://s11.flagcounter.com/count/xOdr/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"
               style={{maxWidth: '100px', maxHeight: '200px'}} />
             </a>
