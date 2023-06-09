11dc37f456e365c9cb9d0919c5b0ef219d922c6c
Byron Fong <byron.fong@gmail.com>
2023-06-08 17:56:00 -0700

    flag counter home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -17,6 +17,13 @@ import Tag from '../components/Tag'
             </a>
             
           </div>
+          <div style={{position: 'absolute', top: 0, right: 0}}>
+            <a href="https://info.flagcounter.com/xOdr">
+              <img src="https://s11.flagcounter.com/count/xOdr/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"
+              style={{maxWidth: '8em', maxHeight: '8em'}} />
+            </a>
+          </div>
+          
         </div>
       )
     }
