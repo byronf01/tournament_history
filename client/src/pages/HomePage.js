beac6f0a3ca514b082cbd44bac52c60a400da69d
Byron Fong <byron.fong@gmail.com>
2023-09-28 19:34:28 -0700

    restore github link

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -11,7 +11,12 @@ import Tag from '../components/Tag'
         <div>
           <Tag />
           <Panel />
-          
+          <div style={{height: '10%', textAlign: 'center', paddingTop: '2em'}}>
+            <a href='https://github.com/byronf01/tournament_history' target="_blank" rel="noreferrer" >
+              <img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' style={{ height: '2.5em', width: 'auto'}}/>
+            </a>
+
+          </div>
           <div style={{position: 'absolute', bottom: 0, right: 0}}>
             <a href="https://info.flagcounter.com/xOdr" target="_blank" rel="noreferrer">
               <img src="https://s11.flagcounter.com/count/xOdr/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"
